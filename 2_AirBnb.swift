021d3aaf98fdb55bb709f2c821879a37dfa1999c
Orkhan Alizada <alizadeorkhan@yahoo.com>
2020-07-09 14:06:44 +0200

    Improvement of the algorithm for the #2 AirBnb problem.

--- 2_AirBnb.swift
+++ 2_AirBnb.swift
@@ -7,13 +7,59 @@ Return -1 if the target is not found.
 let givenArray = [1,3,3,5,7,8,9,9,9,15]
 let target = 9
 
-var firstIndex = -1
-var lastIndex = -1
+class Solution {
+    func searchRange(_ nums: [Int], _ target: Int) -> [Int] {
+        let a = findLowerBound(in: nums, for: target)
+        let b = findUpperBound(in: nums, for: target)
 
-for (index, num) in givenArray.enumerated() {
-    if num == target {
-        firstIndex == -1 ? (firstIndex = index) : (lastIndex = index)
+        return [a, b]
+    }
+
+    func findUpperBound(in nums: [Int], for target: Int) -> Int {
+        var index = -1
+
+        var lowerBound = 0
+        var upperBound = nums.count - 1
+
+        while lowerBound <= upperBound {
+            let mid = lowerBound + (upperBound - lowerBound) / 2
+            let guess = nums[mid]
+
+            if guess == target {
+                index = mid
+            }
+
+            if guess <= target {
+                lowerBound = mid + 1
+            } else {
+                upperBound = mid - 1
+            }
+        }
+
+        return index
+    }
+
+    func findLowerBound(in nums: [Int], for target: Int) -> Int {
+        var index = -1
+
+        var lowerBound = 0
+        var upperBound = nums.count - 1
+
+        while lowerBound <= upperBound {
+            let mid = lowerBound + (upperBound - lowerBound) / 2
+            let guess = nums[mid]
+
+            if guess == target {
+                index = mid
+            }
+
+            if guess >= target {
+                upperBound = mid - 1
+            } else {
+                lowerBound = mid + 1
+            }
+        }
+
+        return index
     }
 }
-
-print(firstIndex, lastIndex)
